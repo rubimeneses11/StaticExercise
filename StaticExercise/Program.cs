e4d4ac1a78e30f5c62158a9405527914ba88711f
Rubi Meneses <rubi.ventura@icloud.com>
2022-08-22 11:40:56 -0500

    done

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using StaticExercise;
+
+//calling Celcius method
+var celcius = TempConverter.FahrenheitToCelsius(32);
+//printing to console
+Console.WriteLine($"Celcius: {celcius}");
+
+//calling fahrenheit method
+var fahrenheit = TempConverter.CelsiusToFahrenheit(0);
+//printing to console
+Console.WriteLine($"Fahrenheit: {fahrenheit}");
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace StaticExercise
+{   //create a static class called TempConverter
+    public static class TempConverter
+    {
+        //create 2 methods, one called FahrenheitToCelsius that will require
+        //a double as a parameter and return a double,
+        //the other CelsiusToFahrenheit which will also require a double as a parameter and return a double.
+        public static double FahrenheitToCelsius (double degree)
+        {
+            return (degree - 32) / 1.8;
+        }
+
+        public static double CelsiusToFahrenheit (double degree)
+        {
+            return (degree * 9) / 5 + 32;
+        }
+    }
+}
+
